94d57362411fa623c5cb21acee6ec0af258d3a14
Alexander Szendy <Alexander Szendy>
2016-11-25 15:06:35 +0100

    Added Group Create Dialog, Group List Dialog.

--- client/src/GroupCreateDialog.java
+++ client/src/GroupCreateDialog.java
@@ -1,27 +1,106 @@
 
+import java.io.File;
+
+import javax.swing.JFileChooser;
+
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.event.ActionEvent;
+import javafx.geometry.Insets;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Dialog;
+import javafx.scene.control.DialogPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.stage.FileChooser;
 import javafx.stage.Window;
 
 public class GroupCreateDialog extends Dialog {
 	public GroupCreateDialog(Window owner){
 		this.initOwner(owner);
 	    this.setResizable(false);
-	    this.setTitle("Add Annotation Type");
+	    this.setTitle("Create Group Formular");
 	    this.getDialogPane().setContent(new CreatePane(this));
 		
 	}
 	private class CreatePane extends GridPane{
 
 		public CreatePane(GroupCreateDialog groupCreateDialog) {
+			DialogPane crtPane = groupCreateDialog.getDialogPane();
+			
+			SimpleBooleanProperty invalidName = new SimpleBooleanProperty();
+	        invalidName.set(true);
+	        SimpleBooleanProperty invalidDoc = new SimpleBooleanProperty();
+	        invalidDoc.set(true);
+	        SimpleBooleanProperty invalidPrivilege = new SimpleBooleanProperty();
+	        invalidPrivilege.set(true);
+			
+			final TextField name = new TextField("");
+			name.textProperty().addListener((ov, oldV, newV) -> {
+               invalidName.set(newV.trim().isEmpty());
+			});
+						
+			final TextArea privileges = new TextArea ("user1:priv; user2:priv;");
+			privileges.textProperty().addListener(e->{
+				invalidPrivilege.set(privileges.getText().isEmpty());
+			});
+			
+			final TextField defaultPriv = new TextField("");
+			final TextField  password= new TextField("");
+			
+			final Button openButton = new Button("Upload File");
+	 
+	        openButton.setOnMouseClicked(e->{
+	        	FileChooser fileChooser = new FileChooser();
+	        	fileChooser.setTitle("Open Resource File");
+	        	fileChooser.getExtensionFilters().addAll(
+	                    new FileChooser.ExtensionFilter("Text Files *.txt", "*.txt")
+	                );
+	        	File file = fileChooser.showOpenDialog(((Node)e.getTarget()).getScene().getWindow());
+                if (file != null) {
+                	invalidDoc.set(file==null);
+                	System.out.println(file.getName());
+                
+                }
+	        });
+			
+			
+			
+			ButtonType okButton = new ButtonType("OK", ButtonBar.ButtonData.OK_DONE);
+		    ButtonType cancelBtn = new ButtonType("Cancel", ButtonBar.ButtonData.CANCEL_CLOSE);
+		    crtPane.getButtonTypes().setAll(okButton, cancelBtn);
+		    Button okBtn = (Button) crtPane.lookupButton(okButton);
+		    okBtn.disableProperty().bind(invalidName.or(invalidPrivilege).or(invalidDoc));
+		    okBtn.addEventFilter(ActionEvent.ACTION, event -> {
+		    	
+		    	
+		    	
+		    	
+		    	
+            });
+			
+			
+			
+		    this.setPadding(new Insets(10));
+            this.setHgap(10);
+            this.setVgap(10);
+            this.add(new Label("Group Name: "), 0, 0);
+            this.add(name, 1, 0);
+            this.add(new Label("Upload Document: "),0,1);
+            this.add(openButton, 1, 1);
+            this.add(new Label("Set User Privileges: "), 0, 2);
+            this.add(privileges, 1, 2);
+            this.add(new Label("Default Privileges: "), 0, 3);
+            this.add(defaultPriv, 1, 3);
+            this.add(new Label("Group Password: "), 0, 4);
+            this.add(password, 1, 4);
 			
 		}
 		
-		
-		
-		
 	}
-	
-	
 
 }
--- client/src/GroupsDialog.java
+++ client/src/GroupsDialog.java
@@ -1,5 +1,13 @@
 
+import javafx.beans.binding.Bindings;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.ContextMenu;
 import javafx.scene.control.Dialog;
+import javafx.scene.control.DialogPane;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Window;
 
@@ -7,11 +15,13 @@ public class GroupsDialog extends Dialog {
 	public GroupsDialog(Window owner){
 		this.initOwner(owner);
 	    this.setResizable(false);
-	    this.setTitle("Add Annotation Type");
+	    this.setTitle("Group List");
 	    this.getDialogPane().setContent(new JoinPane(this));
 	}
 	private class JoinPane extends GridPane{
 		public JoinPane(GroupsDialog groupsDialog) {
+			DialogPane joinPane = groupsDialog.getDialogPane();
+			
 			/* TODO Auto-generated constructor stub
 			 * GroupList
 			 * RightClick :		Join
@@ -23,6 +33,54 @@ public class GroupsDialog extends Dialog {
 			 * Join With Password Dialog
 			 * 
 			 */
+			
+			ListView<String> listView = new ListView<>();
+	        listView.getItems().addAll("One", "Two", "Three");
+
+	        listView.setCellFactory(lv -> {
+
+	            ListCell<String> cell = new ListCell<>();
+
+	            ContextMenu contextMenu = new ContextMenu();
+
+	            //       \"%s\"  == nazov Itemu
+	            MenuItem editItem = new MenuItem();
+	            editItem.textProperty().bind(Bindings.format("Join", cell.itemProperty()));
+	            editItem.setOnAction(event -> {
+	                String item = cell.getItem();
+	                System.out.println("Bind Edit_Item");
+	                event.consume();
+	            });
+	            MenuItem infoItem = new MenuItem();
+	            infoItem.textProperty().bind(Bindings.format("Info", cell.itemProperty()));
+	            infoItem.setOnAction(event -> {
+	                String item = cell.getItem();
+	                System.out.println("Bind Info_Item");
+	            });
+	            MenuItem deleteItem = new MenuItem();
+	            deleteItem.textProperty().bind(Bindings.format("Delete", cell.itemProperty()));
+	            deleteItem.setOnAction(event -> listView.getItems().remove(cell.getItem()));
+	            contextMenu.getItems().addAll(editItem,infoItem, deleteItem);
+
+	            cell.textProperty().bind(cell.itemProperty());
+
+	            cell.emptyProperty().addListener((obs, wasEmpty, isNowEmpty) -> {
+	                if (isNowEmpty) {
+	                    cell.setContextMenu(null);
+	                } else {
+	                    cell.setContextMenu(contextMenu);
+	                }
+	            });
+	            return cell ;
+	        });
+
+	        this.add(listView, 0, 0);
+			
+			
+			
+			ButtonType cancelBtn = new ButtonType("Cancel", ButtonBar.ButtonData.CANCEL_CLOSE);
+		    joinPane.getButtonTypes().setAll(cancelBtn);
+		   
 		}
 	}
 
